2d104e901dab6371025bc8fcb0baf09a8f08d26e
Sophia <sofiya.belous@gmail.com>
2016-09-28 21:53:55 +0300

    added disabled tabs

--- app/pages/common/controllers/companies-dialog.ctrl.js
+++ app/pages/common/controllers/companies-dialog.ctrl.js
@@ -7,5 +7,15 @@
     function CompaniesDialogCtrl($scope, $mdDialog) {
         var vm = this;
         vm.selectedTab = 0;
+        vm.countOfTabs = 4;
+        vm.disabledTabs = [false];
+
+        for (i = 1; i < vm.countOfTabs; i++) {
+            vm.disabledTabs.push(true);
+        }
+
+        $scope.$watch('vm.selectedTab', function() {
+            
+        });
     }
 })();
